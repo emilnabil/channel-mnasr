26403b05478565a9225bd1838c175c8bc813f779
emil-nabil333 <elbrinselomda@yahoo.com>
2025-07-05 12:39:43 +0300

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -1,58 +1,39 @@
-#!/bin/sh # 
- # # Command: wget https://raw.githubusercontent.com/emilnabil/channel-mnasr/main/installer.sh -qO - | /bin/sh # # ########################################### ###########################################  
-MY_URL=https://raw.githubusercontent.com/emilnabil/channel-mnasr/main  
+#!/bin/sh
+#
+# Command:
+# wget https://raw.githubusercontent.com/emilnabil/channel-mnasr/main/installer.sh -qO - | /bin/sh
+#
+# ###############################################################
+
+MY_URL="https://raw.githubusercontent.com/emilnabil/channel-mnasr/main"
+
 echo "******************************************************************************************************************"
-echo "    download and install channel  "
-echo "============================================================================================================================="
-echo " remove old channel "
-# Remove any Channel  # 
-rm -rf /etc/enigma2/*.tv 
-rm -rf /etc/enigma2/*.radio 
-#####################################################################################
-echo "         install channel    "
+echo "        DOWNLOAD AND INSTALL CHANNEL"
+echo "=================================================================================================================="
+
+echo "        REMOVE OLD CHANNELS..."
+rm -rf /etc/enigma2/*.tv
+rm -rf /etc/enigma2/*.radio
+
+echo "        INSTALLING NEW CHANNELS..."
 cd /tmp
-set -e 
-wget -q  "https://raw.githubusercontent.com/emilnabil/channel-mnasr/main/channels_backup_mnasr.tar.gz"
-wait
-tar -xzf channels_backup_mnasr.tar.gz  -C /
-wait
-cd ..
+set -e
+wget -q "${MY_URL}/channels_backup_mnasr.tar.gz"
+tar -xzf channels_backup_mnasr.tar.gz -C /
 set +e
 rm -f /tmp/channels_backup_mnasr.tar.gz
 sleep 2
-echo "" 
-echo "" 
-echo "****************************************************************************************************************************"
-echo "# Channel  INSTALLED SUCCESSFULLY #"
-echo "
-echo " "*********************************************************" 
-	echo "********************************************************************************"
-echo "   UPLOADED BY  >>>>   EMIL_NABIL "   
+
+echo ""
+echo ""
+echo "******************************************************************************************************************"
+echo "# CHANNEL INSTALLED SUCCESSFULLY #"
+echo "*********************************************************"
+echo "********************************************************************************"
+echo "   UPLOADED BY >>>> EMIL_NABIL"
+echo "================================================================================"
 sleep 4
-	echo '========================================================================================================================='
-                                                                           
+
 exit 0
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
